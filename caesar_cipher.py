77dbe4fa17c130ccf8fe94b9be6932648a2bfb8f
Nekrity <149562323+Nekrity@users.noreply.github.com>
2023-11-15 10:02:00 +0200

    improved the code style

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,38 +1,41 @@
-a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def e(t, o): #Encrypt (text, rotation)
-  r = ""
-  for c in t: #text encrypting (t=text)
-    if (a.find(c) == -1): #if can't find simbol then won't replace it
-      r += c
-    else: # else replace a letter by a number equal to a rotation % 26
-      r += (a[(a.find(c) + o) % len(a)])
-  return r
 
-def d(t, o): #Decrypt (text, rotation)
-  r = ""
-  for c in t: #text decrypting (t=text)
-    if (a.find(c) == -1): #if can't find simbol then won't replace it
-      r += c
-    else: # else replace a letter by a number equal to a rotation % 26
-      r += (a[(a.find(c) - o) % len(a)])
-  return r
+def encrypt(text, rotation): #Encrypt (text, rotation)
+    result = ""
+    for caesar in text: #text encrypting (t=text)
+      if (alphabet.find(caesar) == -1): #if can't find simbol then it stays
+        result += caesar
+      else: # else replace a letter by a number equal to a rotation % 26
+        rotated=alphabet.find(caesar) + rotation
+        result += (alphabet[rotated % len(alphabet)])
+    return result
 
-w = """
+def decrypt(text, rotation): #Decrypt (text, rotation)
+    result = ""
+    for caesar in text: #text decrypting (t=text)
+      if (alphabet.find(caesar) == -1): #if can't find simbol then it stays
+        result += caesar
+      else: # else replace a letter by a number equal to a rotation % 26
+        rotated=alphabet.find(caesar) - rotation
+        result += (alphabet[rotated % len(alphabet)])
+    return result
+
+select = """
 1. Encrypt text
 2. Decrypt text
 3. Bruteforce all rotations
 Choose mode: """
-mode = int(input(w))
+mode = int(input(select))
 
 if mode == 1: #if first mode is selected then encrypt text
-  text = input("Enter the text: ")
-  rotation = int(input("Enter the rotation: "))
-  print("Encrypted: " + e(text, rotation))
+    text = input("Enter the text: ")
+    rotation = int(input("Enter the rotation: "))
+    print("Encrypted: " + encrypt(text, rotation))
 elif mode == 2: #if second mode is selected then decrypt text
-  text = input("Enter the text: ")
-  rotation = int(input("Enter the rotation: "))
-  print("Decrypted: " + d(text, rotation))
-elif mode == 3: #if third mode is selected then try to bruteforce all rotations
-  print("Bruteforcing...")
-  print("But I don't know how to do it, sorry ¯\_(ツ)_/¯")
+    text = input("Enter the text: ")
+    rotation = int(input("Enter the rotation: "))
+    print("Decrypted: " + decrypt(text, rotation))
+elif mode == 3: #if third mode is selected then bruteforce all rotations
+    print("Bruteforcing...")
+    print("But I don't know how to do it, sorry ¯\_(ツ)_/¯")
